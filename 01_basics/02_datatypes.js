052ed652d7f0dbdbc038a9c8b4eeff82d3412242
Shivansh2004-Mishra <shivanshmishra0029@gmail.com>
2025-05-30 14:08:18 +0000

    adding conversionOperation.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,6 @@
 "use strict"; //treat all JS code as newer version
 
-// alert( 3 + 3 ) //we are using nodejs , not browser
+// alert( 3 + 3 ) we are using nodejs , not browser
 
 console.log(3+3) // code readability should be high
 
@@ -10,19 +10,16 @@ let name = "Shivansh" // string data type
 let age  =  18        //number
 let isLoggedIn = false//boolean
 
-
-
-// number => 2 to power 53
+// number     =>   2 to power 53
 // bigint
-// string =>""
-// boolean => true/false
-// null=> standalone value
-// undefined =>
-// symbol => unique    
-
+// string     =>   ""
+// boolean    =>   true/false
+// null       =>   standalone value
+// undefined  =>
+// symbol     =>   unique    
 
 // object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
-
+console.log(typeof age);//number
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let score = "33abc"//33,"33","33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"     => 33
+// "33abc"  => NaN
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// true     => 1  ,  "Shivansh"
+// false    => 0  ,  ""
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof someNumber);
+
+
+//******************************* Operation ****************************
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%2);
+
+let str1 = "Shivansh"
+let str2 = " Mishra"
+let str3 = str1 + str2
+
+//console.log(str3);
+//console.log(1 + "2");
+//console.log("1" + 2);
+//console.log("1" + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
