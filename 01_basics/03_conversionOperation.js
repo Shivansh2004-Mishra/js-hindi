17210e9e81b38130708fc34a57f0296ece71cb31
Shivansh2004-Mishra <shivanshmishra0029@gmail.com>
2025-07-16 14:54:22 +0000

    Update in 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,4 +49,12 @@ let str3 = str1 + str2
 //console.log("1" + 2);
 //console.log("1" + "2");
 //console.log("1" + 2 + 2);
-//console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter)
