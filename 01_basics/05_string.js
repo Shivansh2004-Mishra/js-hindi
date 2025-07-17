0b6a1ab4638be71de17bd7aebcd4b9ce727423fa
Shivansh2004-Mishra <shivanshmishra0029@gmail.com>
2025-07-17 11:40:42 +0000

    add initial files for comparison, string manipulation, and datatype summary

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+const name = "Shivansh"
+const repoCount = 12
+
+// console.log(name + repoCount)
+
+console.log(`Hellow my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Shivansh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// **********************************************************************
+
+// Stack (Primitive), Heap(Non primitive)
+
+let myName = "shibbu"
+
+let anotherName = myName
+anotherName = "Shivansh"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "defvrv@gmail.com",
+    upi: "dfvgfv@sbi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "xcv@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
