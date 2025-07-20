ce9edac2f3ce8ba9930d98de433bf3dbcf4fcc6d
Shivansh2004-Mishra <shivanshmishra0029@gmail.com>
2025-07-20 15:20:53 +0000

    add control flow examples including if statements, switch cases, and truthy/falsy values

---
+++ 04_control_flow/one.js
@@ -0,0 +1,57 @@
+// if
+const isUserloggedIn = true
+const temperature = 45
+
+// if (2 == "2"){
+// if (2 === "2"){
+// if (2 != 3){
+// if ( temperature < 40){
+//     console.log("less then 50");  
+// }
+// else {
+// console.log("temperature is greater then 50");
+// }
+
+
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+
+const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+
+
+// const balance = 1000
+
+// if (balance < 500){
+//     console.log("less then 500");
+// }else if(balance < 750){
+//     console.log("less then 750");   
+// }else if(balance < 1000){
+//     console.log("less then 1000");   
+// }else{
+//     console.log("balance => then 1000");   
+// }
+
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle =false
+const loggedInFromEmail = true
+
+if (UserloggedIn && debitCard){
+    console.log("Allow to by cores");
+}
+
+if (loggedInFromEmail || loggedInFromEmail){
+    console.log("user is loggedin");
+}
+
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,10 @@
+// // const userEmail = "Shivansh@google.ai"
+//  const userEmail = ""
+// const userEmail = []
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user name")
+// }
+
+
